1dc8d9f92c308d28759288d96970683a1736dd48
jnoorul <noorul.it@gmail.com>
2017-08-01 07:50:32 +0800

    configuring eslint and babel

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -1,12 +1,14 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/instructions', function(req, res, next) {
+router.get('/instructions', function (req, res, next) {
   res.render('instructions');
 });
 
