3b042a31084dea64391fe69d9efe519fabbdb8c3
jnoorul <noorul.it@gmail.com>
2017-08-04 11:08:16 +0800

    more tests

--- src/routes/trainPlanner.js
+++ src/routes/trainPlanner.js
@@ -11,7 +11,29 @@ router.get('/instructions', (req, res) => {
   res.render('instructions');
 });
 
+function validateEvaluateRequest(req) {
+  const reqData = req.body;
+  const validationResults = [];
+  if (reqData) {
+    if (!reqData.runId) {
+      validationResults.push('runId is mandatory for evaluation');
+    }
+    if (!reqData.teamUrl) {
+      validationResults.push('teamUrl is mandatory for evaluation');
+    }
+    if (!reqData.callbackUrl) {
+      validationResults.push('callbackUrl is mandatory for evaluation');
+    }
+  }
+  return validationResults;
+}
+
 router.post('/evaluate', (req, res) => {
+  const results = validateEvaluateRequest(req);
+  if (results.length > 0) {
+    res.statusCode = '400';
+    res.json({ errors: results });
+  }
   res.json({ message: 'request received' });
 });
 
--- test/acceptance/evaluate.test.js
+++ test/acceptance/evaluate.test.js
@@ -12,14 +12,37 @@ it('evaluate end point should return status code 200', async () => {
   expect(res.body.message).toBe('request received');
 });
 
-it('evaluate should return 400 if invalid json payload is sent', async () => {
+it('evaluate should return 400 if input payload is empty', async () => {
+  const res = await request(app).post('/trainPlanner/evaluate');
+
+  expect(res.statusCode).toBe(400);
+  expect(res.body.errors.length).toBe(3);
+  expect(res.body.errors[0]).toBe('runId is mandatory for evaluation');
+  expect(res.body.errors[1]).toBe('teamUrl is mandatory for evaluation');
+  expect(res.body.errors[2]).toBe('callbackUrl is mandatory for evaluation');
+});
+
+it('evaluate should return 400 if teamUrl is not empty', async () => {
   const res = await request(app).post('/trainPlanner/evaluate')
     .send({
       runId: '123456',
-      teamUrl: '123',
-      callbackUrl: '456',
+      teamUrl: '',
+      callbackUrl: 'http://coordinator.com/result',
     });
 
   expect(res.statusCode).toBe(400);
-  expect(res.body.message).toBe('invalid input received');
+  expect(res.body.errors.length).toBe(1);
+  expect(res.body.errors[0]).toBe('teamUrl is mandatory for evaluation');
+});
+
+it('evaluate should return 400 if teamUrl is not empty', async () => {
+  const res = await request(app).post('/trainPlanner/evaluate')
+    .send({
+      runId: '123456',
+      teamUrl: 'http://www.abc.heroku.com/sorting',
+    });
+
+  expect(res.statusCode).toBe(400);
+  expect(res.body.errors.length).toBe(1);
+  expect(res.body.errors[0]).toBe('callbackUrl is mandatory for evaluation');
 });
