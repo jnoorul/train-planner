ce367227a6774f494da6259b9cc4f8d40ffaf7fc
jnoorul <noorul.it@gmail.com>
2017-08-04 08:50:05 +0800

    deleted idea folder

--- test/acceptance/instructions.test.js
+++ test/acceptance/instructions.test.js
@@ -6,3 +6,10 @@ it('should get instructions page', async () => {
   const res = await request(app).get('/instructions');
   expect(res.statusCode).toBe(200);
 });
+
+it('should get instructions page with data', async () => {
+  const res = await request(app).get('/instructions');
+  expect(res.statusCode).toBe(200);
+  expect(res.type).toBe('text/html');
+  expect(res.text.length).toBeGreaterThan(100);
+});
