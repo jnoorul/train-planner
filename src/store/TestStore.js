75c68f941e3aec4a00afed0082fd0ecb0c7d6bf2
jnoorul <noorul.it@gmail.com>
2017-08-10 08:20:56 +0800

    test for store

--- src/store/TestStore.js
+++ src/store/TestStore.js
@@ -1,21 +1,19 @@
+import fs from 'fs-extra';
+
+
 export default class TestStore {
-  constructor(testCaseDir = `${__dirname}/../../testdata`) {
+  constructor(testCaseDir = `${__dirname}/../../testdata/`) {
     this.testCaseDir = testCaseDir;
   }
-  getTestCases(problemName) {
-    return [
-      {
-        name: `${problemName} + 'test case 1`,
-        input: {},
-        output: {},
-        score: 10,
-      },
-      {
-        name: 'test case 2',
-        input: {},
-        output: {},
-        score: 20,
-      },
-    ];
+  async getTestCases(problemName) {
+    const dirpath = `${this.testCaseDir}${problemName}/`;
+    const testCases = [];
+    const filenames = await fs.readdir(dirpath);
+
+    for (let i = 0; i < filenames.length; i += 1) {
+      const content = await fs.readFile(dirpath + filenames[i]);
+      testCases.push(JSON.parse(content));
+    }
+    return testCases;
   }
 }
