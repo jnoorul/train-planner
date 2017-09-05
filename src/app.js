337f66c1a17b9c8c123e78aef3a6352d89b47d05
jnoorul <noorul.it@gmail.com>
2017-09-05 08:40:41 +0800

    updating endpoints removing /trainPlanner

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/trainPlanner', trainPlanner);
+app.use('/', trainPlanner);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
