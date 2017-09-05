5eddce4ae9212a9a1ef10ee9322442a1036d2e78
jnoorul <noorul.it@gmail.com>
2017-09-05 15:52:36 +0800

    authentication

--- src/service/coordinatorService.js
+++ src/service/coordinatorService.js
@@ -3,7 +3,6 @@ import 'isomorphic-fetch';
 export default async function postResults(url, results) {
   const parts = url.split('://');
   const newUrl = `${parts[0]}://cisadmin:soltandpepper@${parts[1]}`;
-  console.log(`callback url:${newUrl}`);
   return fetch(newUrl, {
     headers: {
       Accept: 'application/json',
