93a52fc23f205ed1e5fa5dde06d769af13befb8d
jnoorul <noorul.it@gmail.com>
2017-09-09 10:20:48 +0800

    logging output

--- src/service/trainPlannerService.js
+++ src/service/trainPlannerService.js
@@ -1,18 +1,18 @@
 import 'isomorphic-fetch';
 
 export default async function execute(url, input) {
-    const problemUrl = url + '/trainPlanner';
-    const response = await fetch(problemUrl, {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(input),
-    });
+  const problemUrl = url + '/trainPlanner';
+  const response = await fetch(problemUrl, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(input),
+  });
 
-    const output = await response.json();
-    console.log('team node url'+problemUrl);
-    console.log('output from team node:'+output);
-    return output;
+  const output = await response.json();
+  console.log(`team node url ${problemUrl}`);
+  console.log(`output from team node: ${JSON.stringify(output)}`);
+  return output;
 }
 
